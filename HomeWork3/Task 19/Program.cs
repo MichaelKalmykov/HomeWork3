04ca018dfc6526f2611d2a9a7c5717b5203ce3e7
Michael <misha060814@gmail.com>
2022-07-07 13:43:08 +0300

    HomeWork3

---
+++ HomeWork3/Task 19/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine ("Введите пятизначное число");
+string number = Console.ReadLine();
+int lenght = number.Length;
+
+if (lenght < 5 || lenght > 5)
+    {
+        Console.WriteLine("Это не пятизначное число");    
+    }
+else
+    {
+        if (number[0]==number[4] || number[1]==number[3])
+            {
+                Console.WriteLine("Ваше число - палиндром.");
+            }
+        else
+            {
+                Console.WriteLine("Ваше число - не палиндром.");
+            }
+    }
